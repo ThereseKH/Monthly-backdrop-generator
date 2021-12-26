7fefa94bff108b9e401a6e69dd43635704e119af
ThereseKH <therese.haugsbo@gmail.com>
2021-12-26 22:57:48 +0100

    Reorganize functions

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+window.addEventListener("load", () => {
+  // Website has loaded and will run the code inside this function
+
+  const downloadImageBtnRef = document.getElementById("download-image-btn");
+  downloadImageBtnRef.addEventListener("click", downloadCalender);
+
+  // Render calendar, todo: move to function
+  const today = new Date();
+
+  const monthNameRef = document.getElementById("month-name");
+  monthNameRef.innerHTML = getMonthNameFromDate(today);
+  const weeksWithDates = generate2dArrayOfDays(today);
+
+  const tbodyRef = document.getElementById("table-body");
+  tbodyRef.innerHTML = "";
+
+  for (const week of weeksWithDates) {
+    const tableRowString = getTableRow(week);
+    tbodyRef.innerHTML += tableRowString;
+  }
+
+  // TODO: Change src of "#background-image" on input change of "#background-url-inp"
+  const backgroundUrlnpRef = document.getElementById("background-url-inp");
+  const backgroundImageRef = document.getElementById("background-image");
+  backgroundUrlnpRef.addEventListener("input", (e) => {
+    const url = e.target.value;
+    backgroundImageRef.src = url;
+    console.debug("settingImageSourceTo");
+  });
+
+  const imgUploadInpRef = document.getElementById("img-upload-inp");
+  console.dir(imgUploadInpRef);
+  imgUploadInpRef.addEventListener("change", (inp) => {
+    if (inp.target.files && inp.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        backgroundImageRef.src = e.target.result;
+      };
+      reader.readAsDataURL(inp.target.files[0]);
+    }
+  });
+});
+
+// TODO: Import font types
+function downloadCalender() {
+  // Use dom to image to convert preview to image
+  console.debug("Start to download");
+  domtoimage
+    .toJpeg(document.getElementById("preview"), { quality: 1.0 })
+    .then(function (dataUrl) {
+      var link = document.createElement("a");
+      link.download = "Your's truly backdrop.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+}
+
 /**
  * Generates a 2d array where week number is on idx 0, monday on 1 , tuesday on 2 etc.
  * @param {Date} date
@@ -81,62 +139,3 @@ function getMonthNameFromDate(date) {
   ];
   return monthNames[monthNum];
 }
-
-function downloadCalender() {
-  // Use dom to image to convert preview to image
-  console.debug("Start to download");
-  domtoimage
-    .toJpeg(document.getElementById("preview"), { quality: 1.0 })
-    .then(function (dataUrl) {
-      var link = document.createElement("a");
-      link.download = "Your's truly backdrop.jpeg";
-      link.href = dataUrl;
-      link.click();
-    });
-}
-
-window.addEventListener("load", () => {
-  // Website has loaded and will run the code inside this function
-
-  const downloadImageBtnRef = document.getElementById("download-image-btn");
-  downloadImageBtnRef.addEventListener("click", downloadCalender);
-
-  // Render calendar, todo: move to function
-  const today = new Date();
-
-  const monthNameRef = document.getElementById("month-name");
-  monthNameRef.innerHTML = getMonthNameFromDate(today);
-  const weeksWithDates = generate2dArrayOfDays(today);
-
-  const tbodyRef = document.getElementById("table-body");
-  tbodyRef.innerHTML = "";
-
-  for (const week of weeksWithDates) {
-    const tableRowString = getTableRow(week);
-    tbodyRef.innerHTML += tableRowString;
-  }
-
-  // TODO: Change src of "#background-image" on input change of "#background-url-inp"
-  const backgroundUrlnpRef = document.getElementById("background-url-inp");
-  const backgroundImageRef = document.getElementById("background-image");
-  backgroundUrlnpRef.addEventListener("input", (e) => {
-    const url = e.target.value;
-    backgroundImageRef.src = url;
-    console.debug("settingImageSourceTo");
-  });
-
-  const imgUploadInpRef = document.getElementById("img-upload-inp");
-  console.dir(imgUploadInpRef);
-  imgUploadInpRef.addEventListener("change", (inp) => {
-    if (inp.target.files && inp.target.files[0]) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        backgroundImageRef.src = e.target.result;
-      };
-      reader.readAsDataURL(inp.target.files[0]);
-    }
-  });
-});
-
-// TODO: Import font types
