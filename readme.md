f9244a505fbe89c2b63c4be077f1f42e78e2c316
ThereseKH <therese.haugsbo@gmail.com>
2021-12-26 22:43:07 +0100

    Initial backdrop generator

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Monthly backdrop generator for iPhone
+
+## Requierments:
+- [ ] choose month and year
+- [ ] include week numbers
+- [ ] customize color of calender
+- [ ] choose background image 
+- [ ] uplod custom image
+- [ ] name of month
+- [ ] font type of month name 
+- [ ] export as image
+- [ ] prewiew for different iPhone screen sizes 
---
+++ script.js
@@ -0,0 +1,144 @@
+/**
+ * Generates a 2d array where week number is on idx 0, monday on 1 , tuesday on 2 etc.
+ * @param {Date} date
+ * @return {number[][]}
+ */
+function generate2dArrayOfDays(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const firstDateOfMonth = new Date(year, month, 1);
+  const firstWeekOfMonth = dayjs(firstDateOfMonth).isoWeek();
+
+  // iterate over dates in month
+  let currentDate = firstDateOfMonth;
+  const dateArray = [];
+  while (currentDate.getMonth() == month) {
+    const week = dayjs(currentDate).isoWeek();
+    const day = currentDate.getDay();
+    const date = currentDate.getDate();
+
+    if (!dateArray[week - firstWeekOfMonth]) {
+      // list of dates in this week does not exist
+      dateArray[week - firstWeekOfMonth] = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ];
+    }
+    dateArray[week - firstWeekOfMonth][0] = week;
+    if (day === 0) {
+      // It is sunday, place date at last index, week will start at monday
+      dateArray[week - firstWeekOfMonth][day + 7] = date;
+    } else {
+      dateArray[week - firstWeekOfMonth][day] = date;
+    }
+
+    currentDate.setDate(date + 1);
+  }
+
+  return dateArray;
+}
+
+/**
+ * Generate html table row from a list of numbers
+ * @param {Number[]} numberArr list of numbers to add to tableRow `tr`
+ * @returns {string} string representation of a table row where each el in numberArr is enclosed in a table cell `td`
+ */
+function getTableRow(numberArr) {
+  // example numberArr = [44, 5, 6, 7, 8]
+
+  let tableCellsString = "";
+  for (const num of numberArr) {
+    tableCellsString += `<td>${num === null ? "" : num}</td>`;
+  }
+  return `<tr>${tableCellsString}</tr>`;
+}
+
+/**
+ * @param {Date} date
+ * @return {string}
+ */
+function getMonthNameFromDate(date) {
+  const monthNum = date.getMonth();
+  const monthNames = [
+    "Januar",
+    "Februar",
+    "Mars",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  return monthNames[monthNum];
+}
+
+function downloadCalender() {
+  // Use dom to image to convert preview to image
+  console.debug("Start to download");
+  domtoimage
+    .toJpeg(document.getElementById("preview"), { quality: 0.95 })
+    .then(function (dataUrl) {
+      var link = document.createElement("a");
+      link.download = "Your's truly backdrop.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+}
+
+// TODO: Render 2d array of dates as a html table
+window.addEventListener("load", () => {
+  // Website has loaded and will run the code inside this function
+
+  const downloadImageBtnRef = document.getElementById("download-image-btn");
+  downloadImageBtnRef.addEventListener("click", downloadCalender);
+
+  // Render calendar, todo: move to function
+  const today = new Date();
+
+  const monthNameRef = document.getElementById("month-name");
+  monthNameRef.innerHTML = getMonthNameFromDate(today);
+  const weeksWithDates = generate2dArrayOfDays(today);
+
+  const tbodyRef = document.getElementById("table-body");
+  tbodyRef.innerHTML = "";
+
+  for (const week of weeksWithDates) {
+    const tableRowString = getTableRow(week);
+    tbodyRef.innerHTML += tableRowString;
+  }
+
+  // TODO: Change src of "#background-image" on input change of "#background-url-inp"
+  const backgroundUrlnpRef = document.getElementById("background-url-inp");
+  const backgroundImageRef = document.getElementById("background-image");
+  backgroundUrlnpRef.addEventListener("input", (e) => {
+    const url = e.target.value;
+    backgroundImageRef.src = url;
+    console.debug("settingImageSourceTo");
+  });
+
+  const imgUploadInpRef = document.getElementById("img-upload-inp");
+  console.dir(imgUploadInpRef);
+  imgUploadInpRef.addEventListener("change", (inp) => {
+    if (inp.target.files && inp.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        backgroundImageRef.src = e.target.result;
+      };
+      reader.readAsDataURL(inp.target.files[0]);
+    }
+  });
+});
+
+// TODO: fetch image from a source
+// TODO: Import font types
